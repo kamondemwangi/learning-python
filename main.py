bb9ca1f6f95281ccfe10618f9c8eaa59d6aeec8c
kamonde Mwangi <39625813+kamondemwangi@users.noreply.github.com>
2020-10-21 08:12:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,23 @@ DNAStr = validateSeq(randDNAStr)
 print(f'\nSequence: {(DNAStr)}\n')
 print(f'[1]  Sequence Length: {len(DNAStr)}\n')
 print(f'[2]  Nucleotide Frequency:{countNucFrequency(DNAStr)}\n')
-print(f'[3]  DNA/RNA Transcription:{transcription(DNAStr)}\n')
-print(f'[4]  Reverse Complement:{reverse_complement(DNAStr)}\n')
+print(f'[3]  DNA/RNA Transcription(mRNA):{transcription(DNAStr)}\n')
 
-print(f"DNA String + Reverse Complement:\n5' {colored(DNAStr)} 3' ")
+print(f"[4] Sense Strand + Complement + Reverse Complement:\n5' {colored(DNAStr)} 3' ")
 print(f"   {''.join(['|' for c in range(len(DNAStr))])}")
-print(f"3' {colored(reverse_complement(DNAStr))} 5' \n")
+print(f"3' {reverse_complement(DNAStr)[::-1]} 5' [Complement]")
+print(f"5' {colored(reverse_complement(DNAStr))} 3' [Rev. Complement] \n")
+
+print(f'[5] GC Content: {gc_content(DNAStr)}%\n')
+print(f'[6] GC Content in sub-sequence K=5: {gc_content_subsec(DNAStr, k=5)}\n')
+
+print(f'[7] Aminoacid Sequence from DNA: {translate_seq(DNAStr, 0)}\n')
+
+print(f'[8] Codon frequency (L): {codon_usage(DNAStr, "L")}\n')
+print(f'[9] Reading frames:')
+for frame in gen_reading_frames(DNAStr):
+    print(frame)
+
+print('\n[10] All proteins in 6 ORFS:')
+for prot in all_proteins_from_orfs(DNAStr, 0, 0, True):
+    print(f'{prot}')
